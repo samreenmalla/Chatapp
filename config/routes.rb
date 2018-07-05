33ee31ca65beb928d87cb81ba088a19cd6ca4a2e
Samreen <Samreen.malla@gmail.com>
2018-07-05 23:00:47 +0545

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :chat_rooms, only: [:new, :create, :show, :index, :update] 
+  root 'chat_rooms#index'
+  mount ActionCable.server => '/cable'
+end
